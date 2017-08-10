a5fb17b8b05235142e78c25061c87567dd242e93
Mahmoud Lababidi <mla@mla.im>
2017-08-10 10:01:59 -0400

    add loop to download

--- buffet_download.py
+++ buffet_download.py
@@ -13,25 +13,29 @@ parser.add_argument("--dryrun", help="Don't download, just list what will be dow
 parser.add_argument("--verbose", help="verbose", action='store_true')
 args = parser.parse_args()
 
+try_again = True
 gbdx = Interface()
-aws = gbdx.s3._load_info()
-os.environ['AWS_ACCESS_KEY_ID'] = aws['S3_access_key']
-os.environ['AWS_SECRET_ACCESS_KEY'] = aws['S3_secret_key']
-os.environ['AWS_SESSION_TOKEN'] = aws['S3_session_token']
+while try_again:
+    try:
+        aws = gbdx.s3._load_info()
+        os.environ['AWS_ACCESS_KEY_ID'] = aws['S3_access_key']
+        os.environ['AWS_SECRET_ACCESS_KEY'] = aws['S3_secret_key']
+        os.environ['AWS_SESSION_TOKEN'] = aws['S3_session_token']
 
+        s3 = sh.aws.bake('s3')
+        s3_uri = "s3://{}/{}/".format(aws['bucket'], aws['prefix'])
 
-s3 = sh.aws.bake('s3')
-s3_uri = "s3://{}/{}/".format(aws['bucket'], aws['prefix'])
-# print(s3("ls", s3_uri))
-
-all_folders = s3("ls", s3_uri).stdout
-if args.verbose:
-    print(all_folders)
-all_folders = regex.findall(regex.escape(args.prefix) + r'[^ ]*/', str(all_folders))
-print(all_folders)
-print(len(all_folders))
-if not args.dryrun:
-    os.makedirs(args.output, exist_ok=True)
-    for folder in all_folders:
-        print(folder)
-        print(s3.sync(s3_uri + folder, args.output + folder))
+        all_folders = s3("ls", s3_uri).stdout
+        if args.verbose:
+            print(all_folders)
+        all_folders = regex.findall(regex.escape(args.prefix) + r'[^ ]*/', str(all_folders))
+        print(all_folders)
+        print(len(all_folders))
+        if not args.dryrun:
+            os.makedirs(args.output, exist_ok=True)
+            for folder in all_folders:
+                print(folder)
+                print(s3.sync(s3_uri + folder, args.output + folder))
+        try_again = False
+    except:
+        continue
